3316d85297367ccfee7156c6447214f4ff11f444
Seil Cho <seilcho7@hotmail.com>
2019-03-30 12:20:29 -0400

    adds skeletons

---
+++ schema.sql
@@ -0,0 +1,7 @@
+create table accounts (
+    id serial primary key,
+    first_name varchar(100),
+    last_name varchar(100),
+    account_id varchar(100),
+    password varchar(500)
+);
---
+++ seed.sql
@@ -0,0 +1,8 @@
+insert into accounts
+    (first_name, last_name, account_id, password)
+VALUES
+    ('Lady', 'Godiva', 'ladyg102', 'nakedtruth!'),
+    ('Nicolas', 'Flamel', 'nicoflame77', 'g3t570n3d'),
+    ('Cruella', 'DeVille', 'cdevil89', 'SpotsSpotsSpots'),
+    ('Lennie', 'Small', 'smalllennie45', 'nonAlgern0n')
+;
